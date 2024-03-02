0739db169b563f56d700cda00592b293f067470d
Owaisk23 <owais1566@gmail.com>
2024-03-03 00:30:03 +0500

    PR changes done

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 console.log("Hello World!")
-console.log("Hello World!")
-console.log("Hello World!")
-console.log("Hello World!")
-console.log("Hello World!")
-console.log("Hello World!")
-console.log("Hello World!")
-console.log("Hello World!")
